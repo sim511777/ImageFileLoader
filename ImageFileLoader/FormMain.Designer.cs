44138fa7c516ab4c6fd28e85eedbd6f426b3770a
sim511777 <sim51177@gmail.com>
2021-08-06 03:06:12 +0900

    초기 커밋.

---
+++ ImageFileLoader/FormMain.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace ImageFileLoader {
+    partial class FormMain {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent() {
+            ShimLib.ImageBoxOption imageBoxOption1 = new ShimLib.ImageBoxOption();
+            this.imgBox = new ShimLib.ImageBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnOpenGdiPlus = new System.Windows.Forms.Button();
+            this.dlgOpen = new System.Windows.Forms.OpenFileDialog();
+            this.btnOpenOpenCV = new System.Windows.Forms.Button();
+            this.btnOpenFreeImage = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // imgBox
+            // 
+            this.imgBox.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.imgBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.imgBox.Location = new System.Drawing.Point(0, 0);
+            this.imgBox.Name = "imgBox";
+            imageBoxOption1.CenterLineColor = System.Drawing.Color.Yellow;
+            imageBoxOption1.InfoFont = ShimLib.EFont.unifont_13_0_06_bdf;
+            imageBoxOption1.RoiRectangleColor = System.Drawing.Color.Blue;
+            imageBoxOption1.TimeCheckCount = 100;
+            imageBoxOption1.UseDrawCenterLine = true;
+            imageBoxOption1.UseDrawCursorInfo = true;
+            imageBoxOption1.UseDrawDebugInfo = true;
+            imageBoxOption1.UseDrawPixelValue = true;
+            imageBoxOption1.UseDrawRoiRectangles = true;
+            imageBoxOption1.UseParallelToDraw = true;
+            this.imgBox.Option = imageBoxOption1;
+            this.imgBox.Size = new System.Drawing.Size(670, 539);
+            this.imgBox.SzPan = new System.Drawing.Size(2, 2);
+            this.imgBox.TabIndex = 0;
+            this.imgBox.Text = "imageBox1";
+            this.imgBox.ZoomLevel = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnOpenFreeImage);
+            this.panel1.Controls.Add(this.btnOpenOpenCV);
+            this.panel1.Controls.Add(this.btnOpenGdiPlus);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(670, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 539);
+            this.panel1.TabIndex = 1;
+            // 
+            // btnOpenGdiPlus
+            // 
+            this.btnOpenGdiPlus.Location = new System.Drawing.Point(6, 12);
+            this.btnOpenGdiPlus.Name = "btnOpenGdiPlus";
+            this.btnOpenGdiPlus.Size = new System.Drawing.Size(93, 23);
+            this.btnOpenGdiPlus.TabIndex = 0;
+            this.btnOpenGdiPlus.Text = "GDI+";
+            this.btnOpenGdiPlus.UseVisualStyleBackColor = true;
+            this.btnOpenGdiPlus.Click += new System.EventHandler(this.btnOpenGdiPlus_Click);
+            // 
+            // dlgOpen
+            // 
+            this.dlgOpen.FileName = "openFileDialog1";
+            this.dlgOpen.Filter = "Image Files(*.BMP;*.JPG;*.PNG;*.TIF)|*.BMP;*.JPG;*.GIF;*.TIF";
+            // 
+            // btnOpenOpenCV
+            // 
+            this.btnOpenOpenCV.Location = new System.Drawing.Point(6, 41);
+            this.btnOpenOpenCV.Name = "btnOpenOpenCV";
+            this.btnOpenOpenCV.Size = new System.Drawing.Size(93, 23);
+            this.btnOpenOpenCV.TabIndex = 1;
+            this.btnOpenOpenCV.Text = "OpenCV";
+            this.btnOpenOpenCV.UseVisualStyleBackColor = true;
+            this.btnOpenOpenCV.Click += new System.EventHandler(this.btnOpenOpenCV_Click);
+            // 
+            // btnOpenFreeImage
+            // 
+            this.btnOpenFreeImage.Location = new System.Drawing.Point(6, 70);
+            this.btnOpenFreeImage.Name = "btnOpenFreeImage";
+            this.btnOpenFreeImage.Size = new System.Drawing.Size(93, 23);
+            this.btnOpenFreeImage.TabIndex = 2;
+            this.btnOpenFreeImage.Text = "FreeImage";
+            this.btnOpenFreeImage.UseVisualStyleBackColor = true;
+            this.btnOpenFreeImage.Click += new System.EventHandler(this.btnOpenFreeImage_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(870, 539);
+            this.Controls.Add(this.imgBox);
+            this.Controls.Add(this.panel1);
+            this.Name = "FormMain";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ShimLib.ImageBox imgBox;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnOpenGdiPlus;
+        private System.Windows.Forms.OpenFileDialog dlgOpen;
+        private System.Windows.Forms.Button btnOpenOpenCV;
+        private System.Windows.Forms.Button btnOpenFreeImage;
+    }
+}
+
---
+++ ImageFileLoader/FormMain.cs
@@ -0,0 +1,178 @@
+﻿using FreeImageAPI;
+using OpenCvSharp;
+using ShimLib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageFileLoader {
+    public partial class FormMain : Form {
+        public FormMain() {
+            InitializeComponent();
+        }
+
+        IntPtr imgBuf = IntPtr.Zero;
+        int imgBw = 0;
+        int imgBh = 0;
+        int imgBitpp = 0;
+        int imgStride = 0;
+        ABufferDrawer drawer = null;
+
+        private void ClearImageBuffer() {
+            if (imgBuf != IntPtr.Zero) {
+                Marshal.FreeHGlobal(imgBuf);
+                imgBuf = IntPtr.Zero;
+            }
+        }
+
+        private void SetImageBuffer() {
+            imgBox.SetImageBuffer(imgBuf, imgBw, imgBh, imgBitpp, imgStride, drawer);
+            imgBox.Invalidate();
+        }
+
+        private static ABufferDrawer BufferDrawerFromBitmap(Bitmap bmp) {
+            var pixelFormat = bmp.PixelFormat;
+            if (pixelFormat == PixelFormat.Format1bppIndexed) {
+                var palette = bmp.Palette?.Entries.Select(color => color.ToArgb()).ToArray();
+                var drawer = new BufferDrawer_1BitIndexed(palette);
+                return drawer;
+            }
+            if (pixelFormat == PixelFormat.Format4bppIndexed) {
+                var palette = bmp.Palette?.Entries.Select(color => color.ToArgb()).ToArray();
+                var drawer = new BufferDrawer_4BitIndexed(palette);
+                return drawer;
+            }
+            if (pixelFormat == PixelFormat.Format8bppIndexed) {
+                var palette = bmp.Palette?.Entries.Select(color => color.ToArgb()).ToArray();
+                var drawer = new BufferDrawer_8BitIndexed(palette);
+                return drawer;
+            }
+            if (pixelFormat == PixelFormat.Format24bppRgb)
+                return new BufferDrawer_24BitRgb();
+            if (pixelFormat == PixelFormat.Format32bppRgb
+                || pixelFormat == PixelFormat.Format32bppPArgb
+                || pixelFormat == PixelFormat.Format32bppArgb)
+                return new BufferDrawer_32BitRgb();
+
+            return new BufferDrawer_Unknown();
+        }
+
+        private static ABufferDrawer BufferDrawerFromMat(MatType mt) {
+            ABufferDrawer drawer = null;
+            if (mt == MatType.CV_8UC1) {
+                drawer = new BufferDrawer_8BitIndexed(null);
+            } else if (mt == MatType.CV_8UC3) {
+                drawer = new BufferDrawer_24BitRgb();
+            } else if (mt == MatType.CV_8UC4) {
+                drawer = new BufferDrawer_32BitRgb();
+            }
+            if (drawer == null)
+                drawer = new BufferDrawer_Unknown();
+
+            return drawer;
+        }
+
+        private static ABufferDrawer BufferDrawerFromFreeImage(FreeImageBitmap fib) {
+            var pixelFormat = fib.PixelFormat;
+            if (pixelFormat == PixelFormat.Format1bppIndexed) {
+                var palette = fib.Palette?.Select(rgbq => (int)rgbq.uintValue).ToArray();
+                var drawer = new BufferDrawer_1BitIndexed(palette);
+                return drawer;
+            }
+            if (pixelFormat == PixelFormat.Format4bppIndexed) {
+                var palette = fib.Palette?.Select(rgbq => (int)rgbq.uintValue).ToArray();
+                var drawer = new BufferDrawer_4BitIndexed(palette);
+                return drawer;
+            }
+            if (pixelFormat == PixelFormat.Format8bppIndexed) {
+                var palette = fib.Palette?.Select(rgbq => (int)rgbq.uintValue).ToArray();
+                var drawer = new BufferDrawer_8BitIndexed(palette);
+                return drawer;
+            }
+            if (pixelFormat == PixelFormat.Format24bppRgb)
+                return new BufferDrawer_24BitRgb();
+            if (pixelFormat == PixelFormat.Format32bppRgb
+                || pixelFormat == PixelFormat.Format32bppPArgb
+                || pixelFormat == PixelFormat.Format32bppArgb)
+                return new BufferDrawer_32BitRgb();
+
+            return new BufferDrawer_Unknown();
+        }
+
+        private void btnOpenGdiPlus_Click(object sender, EventArgs e) {
+            if (dlgOpen.ShowDialog(this) != DialogResult.OK)
+                return;
+
+            ClearImageBuffer();
+
+            using (var bmp = new Bitmap(dlgOpen.FileName)) {
+                drawer = BufferDrawerFromBitmap(bmp);
+                imgBw = bmp.Width;
+                imgBh = bmp.Height;
+                imgBitpp = Image.GetPixelFormatSize(bmp.PixelFormat);
+
+                var bd = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.ReadOnly, bmp.PixelFormat);
+                imgStride = bd.Stride;
+                long bufSize = (long)imgStride * imgBh;
+                imgBuf = Marshal.AllocHGlobal((IntPtr)bufSize);
+                Util.Memcpy(imgBuf, bd.Scan0, bufSize);
+                bmp.UnlockBits(bd);
+            }
+
+            SetImageBuffer();
+        }
+
+        private void btnOpenOpenCV_Click(object sender, EventArgs e) {
+            if (dlgOpen.ShowDialog(this) != DialogResult.OK)
+                return;
+
+            ClearImageBuffer();
+
+            using (var mat = new Mat(dlgOpen.FileName, ImreadModes.Unchanged)) {
+                drawer = BufferDrawerFromMat(mat.Type());
+                imgBw = mat.Width;
+                imgBh = mat.Height;
+                imgBitpp = mat.ElemSize() * 8;
+                imgStride = (int)mat.Step();
+                long bufSize = (long)imgStride * imgBh;
+                imgBuf = Marshal.AllocHGlobal((IntPtr)bufSize);
+                Util.Memcpy(imgBuf, mat.Data, bufSize);
+            }
+
+            SetImageBuffer();
+        }
+
+        private void btnOpenFreeImage_Click(object sender, EventArgs e) {
+            if (dlgOpen.ShowDialog(this) != DialogResult.OK)
+                return;
+
+            ClearImageBuffer();
+
+            using (var fib = new FreeImageBitmap(dlgOpen.FileName)) {
+                drawer = BufferDrawerFromFreeImage(fib);
+                imgBw = fib.Width;
+                imgBh = fib.Height;
+                imgBitpp = fib.ColorDepth;
+                imgStride = fib.Pitch;
+                long bufSize = (long)imgStride * imgBh;
+                imgBuf = Marshal.AllocHGlobal((IntPtr)bufSize);
+                //Util.Memcpy(imgBuf, fib.Bits, bufSize);
+                for (int y = 0; y < imgBh; y++) {
+                    IntPtr psrc = fib.Bits + imgStride * (imgBh - 1 - y);
+                    IntPtr pdst = imgBuf + imgStride * y;
+                    Util.Memcpy(pdst, psrc, imgStride);
+                }
+            }
+
+            SetImageBuffer();
+        }
+    }
+}
